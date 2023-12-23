d143cfd3f34e534c0cac2106f5718dbab45f411f
chelseanirajan <chelseanirajan@gmail.com>
2023-12-23 15:08:45 -0600

    new

--- src/main/java/com/cotiviti/ResourceServer/controller/UserController.java
+++ src/main/java/com/cotiviti/ResourceServer/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.cotiviti.ResourceServer.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.cotiviti.ResourceServer.response.UserRest;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -10,5 +14,16 @@ public class UserController {
     @GetMapping("/status")
     public String getStatusOfTheServer(){
         return "OK";
+    }
+    //@Secured("ROLE_developer")
+    @PreAuthorize("hasAuthority('ROLE_developer') or #id == #jwt.subject")
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt){
+        return "User ID "+ id + " is deleted. With JWT subject "+jwt.getSubject();
+    }
+    @PostAuthorize("hasRole('ROLE_developer') or #returnObject.userId == #jwt.subject")
+    @GetMapping("/{id}")
+    public UserRest getUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt){
+        return new UserRest("9ca9c720-7a53-41ec-942c-f3b2887ab580", "Nirajan", "Karki");
     }
 }
--- src/main/java/com/cotiviti/ResourceServer/security/SpringSecurityAuth2.java
+++ src/main/java/com/cotiviti/ResourceServer/security/SpringSecurityAuth2.java
@@ -3,11 +3,13 @@ package com.cotiviti.ResourceServer.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityAuth2{
